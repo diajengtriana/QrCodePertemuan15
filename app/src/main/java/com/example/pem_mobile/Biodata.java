085f72c1c10452a8d5be820e52e7ddc62232d2c2
diajengtriana <diajengtriana11@gmail.com>
2023-01-11 23:00:11 +0700

    upload

---
+++ app/src/main/java/com/example/pem_mobile/Biodata.java
@@ -0,0 +1,6 @@
+package com.example.pem_mobile;
+
+import android.app.Activity;
+
+public class Biodata extends Activity {
+}
---
+++ app/src/main/java/com/example/pem_mobile/Tentang.java
@@ -0,0 +1,6 @@
+package com.example.pem_mobile;
+
+import android.app.Activity;
+
+public class Tentang extends Activity {
+}
